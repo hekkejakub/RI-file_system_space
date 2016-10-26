ad0f47b2372b50b0a822ab22a0f7a97f08348efc
james <james@localhost.localdomain>
2016-10-26 11:48:47 +0200

    initial

---
+++ src/ri_java_file_system_space/RI_java_file_system_space.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ri_java_file_system_space;
+
+/**
+ *
+ * @author james
+ */
+public class RI_java_file_system_space {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
